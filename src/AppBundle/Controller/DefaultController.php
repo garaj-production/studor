27bcd3e4073b0da7538a70893d82b63cbe968523
dmnbars <dmnbars@gmail.com>
2017-07-08 12:18:59 +0300

    doctrine-migrations-bundle

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -12,7 +12,6 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        // replace this example code with whatever you need
         return $this->render('default/index.html.twig');
     }
 }
