7266ed6b4ccfac3869b789fdc3f60242dd2db1f0
dmnbars <dmnbars@gmail.com>
2017-07-17 23:18:27 +0300

    default users table

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -3,7 +3,12 @@
 namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="app_user")
+ */
 class User extends BaseUser
 {
     /**
