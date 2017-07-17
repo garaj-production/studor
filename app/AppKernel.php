7a0eddfc87ce6a9b3d82e4c7bbd0a7d4aa80b151
dmnbars <dmnbars@gmail.com>
2017-07-17 23:12:39 +0300

    default fosuserbundle install

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new \Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
 
+            new \FOS\UserBundle\FOSUserBundle(),
+
             new AppBundle\AppBundle(),
         ];
 
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+}
