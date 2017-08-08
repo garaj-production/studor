d506d0485b2e73b6facd71e6e9ef5a3f453c5bd6
dmnbars <dmnbars@gmail.com>
2017-08-08 17:34:06 +0300

    deploy v1

---
+++ deploy.php
@@ -0,0 +1,28 @@
+<?php
+namespace Deployer;
+
+require 'recipe/symfony3.php';
+
+// Configuration
+
+set('repository', 'git@github.com:ahatojli4/studor.git');
+set('git_tty', true); // [Optional] Allocate tty for git on first deployment
+set('default_stage', 'demo');
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+host('green.elastictech.org')
+    ->user('u3087')
+    ->stage('demo')
+    ->set('deploy_path', '/home/u3087/domains/u3087.green.elastictech.org');
+
+// Tasks
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'database:migrate');
