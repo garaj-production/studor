7b64157aad4f47830cc19cfae4795bf599bfadee
dmnbars <dmnbars@gmail.com>
2017-08-15 11:31:42 +0300

    build static on deploy

--- deploy.php
+++ deploy.php
@@ -1,12 +1,16 @@
 <?php
+
 namespace Deployer;
 
 require 'recipe/symfony3.php';
+require 'vendor/deployphp/recipes/rsync.php';
 
 // Configuration
 
-set('repository', 'git@github.com:ahatojli4/studor.git');
-set('git_tty', true); // [Optional] Allocate tty for git on first deployment
+set('repository', 'https://github.com/ahatojli4/studor.git');
+set('git_tty', true);
+set('ssh_type', 'native');
+set('ssh_multiplexing', true);
 set('keep_releases', 2);
 set('default_stage', 'demo');
 
@@ -14,19 +18,39 @@ add('shared_files', []);
 add('shared_dirs', []);
 add('writable_dirs', []);
 
+// static start
+set('rsync',[
+    'exclude'      => [],
+    'exclude-file' => false,
+    'include'      => [],
+    'include-file' => false,
+    'filter'       => [],
+    'filter-file'  => false,
+    'filter-perdir'=> false,
+    'flags'        => 'rz', // Recursive, with compress
+    'options'      => ['delete'],
+    'timeout'      => 60,
+]);
+
+set('clear_paths', ['web/assets/']);
+set('rsync_src', __DIR__ . '/web/assets/');
+set('rsync_dest','{{release_path}}/web/assets/');
+
+task('build_static', function() {
+    runLocally('webpack');
+});
+// static end
+
 // Hosts
-host('green.elastictech.org')
+server('demo', 'green.elastictech.org')
     ->user('u3087')
-    ->stage('demo')
     ->set('env', 'prod')
     ->set('http_user', 'u3087')
     ->set('deploy_path', '/home/u3087/domains/u3087.green.elastictech.org');
 
 // Tasks
+before('rsync', 'build_static');
+after('deploy:clear_paths', 'rsync');
 
-// [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
-
-// Migrate database before symlink new release.
-
 before('deploy:symlink', 'database:migrate');
