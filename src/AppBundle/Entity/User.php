94001a7bf877795d1e51390e401c5b916b07e3cc
dmnbars <dmnbars@gmail.com>
2017-09-02 16:27:10 +0300

    add avatar property

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -4,10 +4,14 @@ namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="app_user")
+ *
+ * @Vich\Uploadable
  */
 class User extends BaseUser
 {
@@ -19,6 +23,20 @@ class User extends BaseUser
     protected $id;
 
     /**
+     * @Vich\UploadableField(mapping="avatar", fileNameProperty="avatarName")
+     *
+     * @var File
+     */
+    private $avatar;
+
+    /**
+     * @ORM\Column(name="avatar", type="string", length=255)
+     *
+     * @var string
+     */
+    private $avatarName;
+
+    /**
      * @inheritdoc
      */
     public function setEmail($email)
@@ -28,5 +46,31 @@ class User extends BaseUser
         $this->setUsername($email);
 
         return $this;
+    }
+
+    public function setAvatar(File $avatar = null)
+    {
+        $this->avatar = $avatar;
+    }
+
+    /**
+     * @return File|null
+     */
+    public function getAvatar()
+    {
+        return $this->avatar;
+    }
+
+    public function setAvatarName(string $avatarName)
+    {
+        $this->avatarName = $avatarName;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getAvatarName()
+    {
+        return $this->avatarName;
     }
 }
