36184b0b15436abb2a4529346793534815261652
dmnbars <dmnbars@gmail.com>
2017-09-14 22:46:29 +0300

    redirect to home page from registartion form if user already authorizated

--- src/AppBundle/EventSubscriber/RegistrationSubscriber.php
+++ src/AppBundle/EventSubscriber/RegistrationSubscriber.php
@@ -3,19 +3,46 @@
 namespace AppBundle\EventSubscriber;
 
 use FOS\UserBundle\Event\FormEvent;
+use FOS\UserBundle\Event\GetResponseUserEvent;
 use FOS\UserBundle\FOSUserEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class RegistrationSubscriber implements EventSubscriberInterface
 {
     /** @var UrlGeneratorInterface */
     private $router;
+    /** @var AuthorizationCheckerInterface */
+    private $authorizationChecker;
 
-    public function __construct(UrlGeneratorInterface $router)
-    {
+    public function __construct(
+        UrlGeneratorInterface $router,
+        AuthorizationCheckerInterface $authorizationChecker
+    ) {
         $this->router = $router;
+        $this->authorizationChecker = $authorizationChecker;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            FOSUserEvents::REGISTRATION_SUCCESS => 'onRegistrationSuccess',
+            FOSUserEvents::REGISTRATION_INITIALIZE => 'onRegistrationInitialize',
+        ];
+    }
+
+    public function onRegistrationInitialize(GetResponseUserEvent $event)
+    {
+        if ('POST' === $event->getRequest()->getMethod()) {
+            return;
+        }
+
+        if ($this->authorizationChecker->isGranted('ROLE_USER')) {
+            $url = $this->router->generate('homepage');
+            $event->setResponse(new RedirectResponse($url));
+        }
     }
 
     public function onRegistrationSuccess(FormEvent $event)
@@ -23,16 +50,6 @@ class RegistrationSubscriber implements EventSubscriberInterface
         $url = $this->router->generate('homepage');
 
         $event->setResponse(new RedirectResponse($url));
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public static function getSubscribedEvents()
-    {
-        return [
-            FOSUserEvents::REGISTRATION_SUCCESS => 'onRegistrationSuccess',
-        ];
     }
 }
 
