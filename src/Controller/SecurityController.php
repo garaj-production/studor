bb21dbc264e02bb38c90d6e14ee080bf47597a13
dmnbars <dmnbars@gmail.com>
2017-12-12 23:54:08 +0300

    remove ugly hack for user password encoder

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,7 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends Controller
@@ -39,11 +39,11 @@ class SecurityController extends Controller
      * @Route("/registration", name="registration")
      *
      * @param Request $request
-     * @param UserPasswordEncoderInterface $encoder
+     * @param PasswordEncoderInterface $encoder
      *
      * @return Response
      */
-    public function registration(Request $request, UserPasswordEncoderInterface $encoder): Response
+    public function registration(Request $request, PasswordEncoderInterface $encoder): Response
     {
         $userRegistrationDTO = new UserRegistrationDTO();
         $form = $this->createForm(UserRegistrationType::class, $userRegistrationDTO);
@@ -53,18 +53,14 @@ class SecurityController extends Controller
             $user = new User(
                 $userRegistrationDTO->name,
                 $userRegistrationDTO->email,
-                // @todo: fix this ugly hack
-                $encoder->encodePassword(new User('', '', ''), $userRegistrationDTO->plainPassword)
+                $encoder->encodePassword($userRegistrationDTO->plainPassword, '')
             );
 
             $manager = $this->getDoctrine()->getManager();
             $manager->persist($user);
             $manager->flush();
 
-            $this->addFlash(
-                'success',
-                'Вы успешно зарегистрировались'
-            );
+            $this->addFlash('success', 'Вы успешно зарегистрировались');
 
             return $this->redirectToRoute('login');
         }
