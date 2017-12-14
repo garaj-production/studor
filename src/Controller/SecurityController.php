0e59b557ad3ef0fd87dccac45108c9e4dd9b96e5
dmnbars <dmnbars@gmail.com>
2017-12-15 00:03:00 +0300

    auth after register

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,6 +9,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -43,7 +45,7 @@ class SecurityController extends Controller
      *
      * @return Response
      */
-    public function registration(Request $request, PasswordEncoderInterface $encoder): Response
+    public function registration(Request $request, PasswordEncoderInterface $encoder, TokenStorageInterface $tokenStorage): Response
     {
         $userRegistrationDTO = new UserRegistrationDTO();
         $form = $this->createForm(UserRegistrationType::class, $userRegistrationDTO);
@@ -60,9 +62,12 @@ class SecurityController extends Controller
             $manager->persist($user);
             $manager->flush();
 
+            $token = new UsernamePasswordToken($user, null, 'main', $user->getRoles());
+            $tokenStorage->setToken($token);
+
             $this->addFlash('success', 'Вы успешно зарегистрировались');
 
-            return $this->redirectToRoute('login');
+            return $this->redirectToRoute('homepage');
         }
 
         return $this->render('security/registration.html.twig', ['form' => $form->createView()]);
